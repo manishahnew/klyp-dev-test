6299283c87f86b024c925bbba7551c4e84d1e974
Mani <maani.cr7@gmail.com>
2022-07-13 22:16:40 +1000

    set up plugin header

---
+++ klyp.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Klyp - Developer Test
+Plugin URI: https://klyp.co/
+Description: Developer test for Klyp. 
+Version: 1.0.0
+Author: Klyp
+Author URI: https://klyp.co/
+License: GPLv2 or later
+Text Domain: klyp
+*/
